52916dbbc02ea18b529f0a06ea2c094c09c25747
Umer-FarooqAhmed <123248058+Umer-Farooq-Ahmed@users.noreply.github.com>
2023-01-26 01:29:56 +0500

    add.Js
    add.js

---
+++ app.js
@@ -0,0 +1,46 @@
+// **** Chapter no 1
+
+
+// Question no 1
+
+//alert("Wellcome My JavaScript Assignment")
+
+
+// Question no 2
+
+
+// var name ="Error!Please enter a valid password"
+
+// alert(name)
+
+
+// Question no 3
+
+
+//var page ="Welcome to JS land... /n Happy Coding!"
+
+
+
+//alert(page)
+
+
+// var fullname="Welcome to JS land..."
+
+
+// alert(fullname)
+
+// fullname="Happy Coding"
+
+// alert(fullname)
+
+
+// Queston no 5
+
+
+// alert("Hello... I can run JS through my web browser's console")
+
+
+// Question no 6
+
+
+alert("Question use Html and Css")
